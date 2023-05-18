39811b332b35c929ad50e250464bd2daca884e56
pubudidev <pubudidev@gmail.com>
2023-05-18 09:42:59 -0700

    Update table view cell

--- BeFitFitness/Views/ColletionTableViewCell.swift
+++ BeFitFitness/Views/ColletionTableViewCell.swift
@@ -13,6 +13,16 @@ class ColletionTableViewCell: UITableViewCell {
     // to identify this UITableViewCell
     static let identifire = "ColletionViewTableViewCell"
     
+    private let collectionView: UICollectionView = {
+        let layout  = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 140, height: 200)
+        let collectioView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectioView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectioView
+    }()
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -25,9 +35,33 @@ class ColletionTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
+}
+
+extension ColletionTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell" , for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    // 10 from left to right
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
 }
